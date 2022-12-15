d5ed712b9705fdfe7141291a9ee96664b0d7b0be
Thomas Kinsella <thomasjkinsella4@gmail.com>
2022-12-15 17:56:23 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Main from './Main';
+import NavBar from './NavBar';
+import video from "./websiteteaservid.mp4";
+// import logo from "./logoColors/vintagelogo.png";
+
+// THINGS TO LOOK INTO -
+    // - SUBSCRIBE TO EMAILS
+    // - OVERALL LAYOUT
+    // - BLOG LAYOUT, MODAL??
+    // - SHOPIFY
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Main/>} />
+    //     <Route path="/test" element={<Test/>} />
+    //   </Routes>
+    // </Router>
+    <>
+    <div id="nav-logo-div">
+      {/* <img id="vintage-logo" src={logo}></img> */}
+      <video id="header-vid" src={video} type="video/mp4" loop={true} muted={true} autoPlay={true} playsInline={true}></video>
+      {/* <video id="header-vid" src={video} autoPlay loop muted></video>
+      <video id="header-vid" src={video} autoPlay loop muted></video>
+      <video id="header-vid" src={video} autoPlay loop muted></video> */}
     </div>
+    <NavBar />
+    <Main />
+    </>
   );
 }
 
 export default App;
+
+
+
